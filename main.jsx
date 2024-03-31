b8d31bc5f09b2f4d38d686b8ab7752b2c1f84914
Sarah <vanwars@gmail.com>
2024-03-31 15:28:21 -0400

    Added configuration info

--- main.jsx
+++ main.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
-async function main() {
+function main() {
     const rootEl = document.getElementById("app");
     const root = createRoot(rootEl);
     root.render(<App />);
 }
 
-await main();
+main();
